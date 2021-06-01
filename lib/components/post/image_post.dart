9d1406c86259288214f7815a150441626eccfe0c
Mathias Zunino <mathiaszunino@gmail.com>
2021-05-31 22:07:04 -0300

    feat(post): image post

---
+++ lib/components/post/image_post.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/models/post.dart';
+
+class ImagePost extends StatelessWidget {
+  final Post post;
+  final List<Widget> content;
+
+  const ImagePost({Key key, @required this.post, this.content})
+      : super(key: key);
+
+  List<Widget> getContent() {
+    List<Widget> formatedContent = content.map((item) => item).toList();
+    formatedContent.add(
+      new Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(4),
+          child: Image.network(post.url),
+        ),
+      ),
+    );
+    return formatedContent;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 8,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: getContent(),
+        ),
+      ),
+    );
+  }
+}
