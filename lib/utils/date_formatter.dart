a718743b128d9e0a9ac28370dccb7efe086491ee
Mathias Zunino <mathiaszunino@gmail.com>
2021-07-01 22:24:45 -0300

    feat(chat): date formatter

---
+++ lib/utils/date_formatter.dart
@@ -0,0 +1,34 @@
+import 'package:intl/intl.dart';
+
+class DateFormatter {
+  String getVerboseDateTime(DateTime dateTime) {
+    DateTime now = DateTime.now();
+    DateTime justNow = DateTime.now().subtract(Duration(minutes: 1));
+    DateTime localDateTime = dateTime.toLocal();
+
+    if (!localDateTime.difference(justNow).isNegative) {
+      return 'Just now';
+    }
+
+    String roughTimeString = DateFormat('jm').format(dateTime);
+    if (localDateTime.day == now.day &&
+        localDateTime.month == now.month &&
+        localDateTime.year == now.year) {
+      return roughTimeString;
+    }
+
+    DateTime yesterday = now.subtract(Duration(days: 1));
+    if (localDateTime.day == yesterday.day &&
+        localDateTime.month == yesterday.month &&
+        localDateTime.year == yesterday.year) {
+      return 'Yesterday, ' + roughTimeString;
+    }
+
+    if (now.difference(localDateTime).inDays < 4) {
+      String weekday = DateFormat('EEEE').format(localDateTime);
+      return '$weekday, $roughTimeString';
+    }
+
+    return '${DateFormat('yMd').format(dateTime)}';
+  }
+}
