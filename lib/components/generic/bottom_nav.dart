50da99739709db03beb7e659ea296d25d85e9baf
Mathias Zunino <mathiaszunino@gmail.com>
2021-07-08 21:57:18 -0300

    fix(notifications): notification icon

--- lib/components/generic/bottom_nav.dart
+++ lib/components/generic/bottom_nav.dart
@@ -113,7 +113,7 @@ class _BottomNavState extends State<BottomNav> {
                       ],
                     );
                   }
-                  return Container();
+                  return Icon(Icons.notifications);
                 }),
           ),
           BottomNavigationBarItem(
